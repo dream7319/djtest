b57f32c2c7546dbcffcedb0c8f1f58ff37ab6795
lierl <lierlei0515@163.com>
2018-03-22 23:05:47 +0800

    add

---
+++ demo/aa.py
@@ -0,0 +1,6 @@
+from class_demo import A
+from class_demo import cc
+def bb():
+    A.aa()
+    cc()
+bb()
---
+++ demo/class_demo.py
@@ -0,0 +1,9 @@
+
+class A(object):
+    def __init__(self):
+        print('aa')
+    def aa():
+        print('bb')
+
+def cc():
+    print("cc")
